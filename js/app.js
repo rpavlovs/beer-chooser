b44b4f114a1c2a4350c93a644c28d6853e7ab14c
Roman Pavlovskyi <wilohvost@gmail.com>
2015-10-04 17:55:26 -0400

    Change internal logic to support deeper caching. Make a view that is shown when no new beer is left.

--- js/app.js
+++ js/app.js
@@ -3,10 +3,11 @@ LCBO_API_LOCAL_BEER       = "http://lcboapi.com/products?q=beer&store_id=511&cal
 LCBO_API_LOCAL_INVENTORY  = "http://lcboapi.com/inventories?store_id=511&callback=JSON_CALLBACK";
 LCBO_API_PRODUCTS         = "http://lcboapi.com/products/";
 
+NUM_BEERS_TO_PRELOAD      = 10;
 
-var app = angular.module('beerChooser', ["firebase", 'ui.bootstrap']);
+var app = angular.module('beerChooser', ["firebase", 'ui.bootstrap'])
 
-app.filter('reverse', function() {
+.filter('reverse', function() {
    function toArray(list) {
     var k, out = [];
     if( list ) {
@@ -24,58 +25,112 @@ app.filter('reverse', function() {
   return function(items) {
     return toArray(items).slice().reverse();
   };
-});
+})
 
-app.controller('BeerListCtrl', function($scope, $http, $q, $firebaseArray) {
-  var ref = new Firebase("https://glowing-heat-4629.firebaseio.com/"),
-      local_beer_ids = [];
+.controller('BeerListCtrl', function($scope, $http, $q, $firebaseArray) {
+  var ref = new Firebase("https://glowing-heat-4629.firebaseio.com/");
 
+  $scope.new_beer_ids = [];
   $scope.past_beers = $firebaseArray(ref);
+  $scope.next_beers_cache = [];
+  $scope.isLoading = true;
+  $scope.num_beers_to_try = 0;
 
   $q.all([
       getLocalBeerIdsAsync(),
       $scope.past_beers.$loaded()
     ])
     .then(function(resolve){
-      local_beer_ids = resolve[0];
-      return getNewRandomLocalBeerAsync();
-    })
-    .then(function (beer) {
-      $scope.curr_beer = beer;
-      return getNewRandomLocalBeerAsync();
-    })
-    .then(function (beer) {
-      $scope.next_beer = beer;
-      console.log("Past beers: ", $scope.past_beers);
+      var past_beer_ids = $scope.past_beers.map(function (beer) {return beer.id;});
+
+      $scope.new_beer_ids = _.difference(resolve[0], past_beer_ids);
+
+      console.log("New beer ids: ", $scope.new_beer_ids);
+
+      if ($scope.new_beer_ids.length < 1) {
+        $scope.isLoading = false;
+        return;
+      }
+
+      $scope.new_beer_ids.shuffle();
+      console.log("New beer ids shuffled: ", $scope.new_beer_ids);
+
+      $scope.num_beers_to_try = $scope.new_beer_ids.length;
+
+      getProductAsync($scope.new_beer_ids.shift())
+        .then(function(beer){
+          $scope.next_beers_cache[0] = beer;
+          $scope.isLoading = false;
+        });
+
+      var next_beers_promices = [];
+
+      for (var i=0; i<NUM_BEERS_TO_PRELOAD-1 && $scope.new_beer_ids[0] ; i++) {
+        next_beers_promices.push(getProductAsync($scope.new_beer_ids.shift()));
+      }
+
+      $q.all(next_beers_promices)
+        .then(function (beers) {
+          $scope.next_beers_cache = $scope.next_beers_cache.concat(beers);
+          console.log("Next beers cache:", $scope.next_beers_cache);
+        });
     });
 
   $scope.acceptBeer = function() {
-    $scope.past_beers.$add($scope.curr_beer)
-      .then(function () {
-        console.log("past_beers acceptBeer added", $scope.past_beers);
-        $scope.showAnotherBeer();
+    $scope.num_beers_to_try--;
+    $scope.past_beers.$add($scope.next_beers_cache.shift())
+      .then(function() {
+        console.log("Accept, past_beers updated", $scope.past_beers);
+        console.log("$scope.new_beer_ids", $scope.new_beer_ids);
+        console.log("$scope.next_beers_cache", $scope.next_beers_cache);
+
+        if ($scope.new_beer_ids.length <= 0) {
+          return $q.reject();
+        }
+        return getProductAsync($scope.new_beer_ids.shift());
+      })
+      .then(function(beer) {
+         $scope.next_beers_cache.push(beer);
       });
   };
 
   $scope.showAnotherBeer = function() {
-    $scope.curr_beer = $scope.next_beer;
+    console.log($scope.next_beers_cache);
 
-    getNewRandomLocalBeerAsync($scope.past_beers)
-      .then(function (beer) {
-        console.log(beer);
-        $scope.next_beer = beer;
-      });
+    if ($scope.new_beer_ids.length > 0) {
+      getProductAsync($scope.new_beer_ids.shift())
+        .then(function (beer) {
+          $scope.next_beers_cache.push(beer);
+        });
+
+      $scope.new_beer_ids.push($scope.next_beers_cache.shift().id);
+      $scope.new_beer_ids.shuffle();
+    }
+    else {
+      var prev_beer = $scope.next_beers_cache[0];
+      while (prev_beer === $scope.next_beers_cache[0]) {
+        $scope.next_beers_cache.shuffle();
+      }
+    }
   };
 
   $scope.startOver = function() {
     ref.remove();
+    $scope.past_beers.forEach(function(beer){
+      console.log(beer);
+      $scope.new_beer_ids.push(beer.id);
+    });
+    $scope.num_beers_to_try = $scope.new_beer_ids.length + $scope.next_beers_cache.length;
+    console.log("$scope.new_beer_ids", $scope.new_beer_ids);
+    console.log("$scope.next_beers_cache", $scope.next_beers_cache);
+
   };
 
-  function getProductAsync(id) {
+  function getProductAsync(id, cache) {
     return $q(function(resolve, reject) {
       $http.jsonp(LCBO_API_PRODUCTS+id+"?callback=JSON_CALLBACK")
         .then(function (responce) {
-          console.log(responce);
+          console.log("Load "+id+":", responce);
           var product = responce.data.result;
           if (product.image_url === null) {
             product.image_url = "img/no_image_bottle.png";
@@ -88,14 +143,6 @@ app.controller('BeerListCtrl', function($scope, $http, $q, $firebaseArray) {
           resolve(product);
         });
     });
-  }
-
-  function getNewRandomLocalBeerAsync() {
-    var past_beer_ids = $scope.past_beers.map(function (beer) {return beer.id;}),
-        new_beer_ids = _.difference(local_beer_ids, past_beer_ids),
-        random_index = Math.floor(Math.random()*new_beer_ids.length);
-    $scope.num_beers_to_try = new_beer_ids.length;
-    return getProductAsync(new_beer_ids[random_index]);
   }
 
   function getLocalBeerIdsAsync() {
@@ -149,3 +196,19 @@ app.controller('BeerListCtrl', function($scope, $http, $q, $firebaseArray) {
     });
   }
 });
+
+Array.prototype.shuffle = function() {
+    var counter = this.length, temp, index;
+
+    while (counter > 0) {
+        index = Math.floor(Math.random() * counter);
+
+        counter--;
+
+        temp = this[counter];
+        this[counter] = this[index];
+        this[index] = temp;
+    }
+
+    return this;
+};
